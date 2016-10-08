0b49f90cfbbe0475c7639d2fa687c529de25470f
Jason Lane <quizathaderat@yahoo.com>
2016-10-08 13:59:56 -0700

    Begin adding bullets to ship

---
+++ bullet.py
@@ -0,0 +1,32 @@
+import pygame
+from pygame.sprite import sprite
+
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship"""
+
+    def __init__(self, game_settings, screen, ship):
+        """Create a bullet at the ship's current position"""
+        super.__init__()
+        self.screen = screen
+
+        # Create a bullet rect at (0, 0) and then corrent position
+        self.rect = pygame.Rect(0, 0, game_settings.bullet_width, game_settings.bullet_height)
+        self.rect.centerx = ship.rect.centerx
+        self.rect.top = ship.rect.top
+
+        # Store the bullet's position as a decimal value
+        self.y = float(self.rect.y)
+
+        self.color = game_settings.bullet_color
+        self.speed_factor = game_settings.bullet_speed_factor
+
+    def update(self):
+        """Move the bullet up the screen"""
+        # Update the decimal position of the bullet
+        self.y -= self.speed_factor
+        # Update the rect of the bullet
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Draw the bullet to the screen"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
