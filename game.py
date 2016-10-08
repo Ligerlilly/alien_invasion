812348c4cd48d34308b2200559785000df54aa7d
Jason Lane <quizathaderat@yahoo.com>
2016-10-08 13:26:18 -0700

    Add ship can move left and right

--- game.py
+++ game.py
@@ -22,6 +22,7 @@ def run_game():
     # Start the main loop for the game
     while True:
         gf.check_events(ship)
+        ship.update()
         gf.update_screen(game_settings, screen, ship)
 
 run_game()
